20abd6ba7955d21b1663628b24d3fa58e524339c
James Alexander <60295314+Jalexander01@users.noreply.github.com>
2021-06-10 09:45:27 -0500

    update

--- src/components/Card.component.jsx
+++ src/components/Card.component.jsx
@@ -7,15 +7,11 @@ function Card(props) {
       <div className="card">
         <div className="top">
           <h2 className="name">{props.name}</h2>
-          <img
-            className="circle-img"
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
+          <img className="circle-img" src={props.img} alt="avatar_img" />
         </div>
         <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
+          <p>{props.tel}</p>
+          <p>{props.email}</p>
         </div>
       </div>
     </div>
